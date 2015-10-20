e212ed1236313714f01acc42603ba4ae831cbabc
Alexei <alexeiatyahoodotcom@gmail.com>
2015-10-20 12:45:56 -0400

    Add WYSIWYG editor to home page.

--- localore/home/models.py
+++ localore/home/models.py
@@ -1,9 +1,15 @@
 from __future__ import unicode_literals
 
-from django.db import models
+#from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full")
+    ]
