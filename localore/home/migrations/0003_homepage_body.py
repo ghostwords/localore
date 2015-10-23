e264189eab0e469fd083462525870afe56b069e4
Alexei <alexeiatyahoodotcom@gmail.com>
2015-10-23 16:03:36 -0400

    Forgot to commit migration for e212ed1.

---
+++ localore/home/migrations/0003_homepage_body.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import wagtail.wagtailcore.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('home', '0002_create_homepage'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='homepage',
+            name='body',
+            field=wagtail.wagtailcore.fields.RichTextField(blank=True),
+        ),
+    ]
