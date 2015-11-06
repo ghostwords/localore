225d27211fb949a74465651df5fdb9153608ff2f
Alexei <alexeiatyahoodotcom@gmail.com>
2015-11-06 16:01:09 -0500

    Fix offline asset compression.

--- localore/blog/wagtail_hooks.py
+++ localore/blog/wagtail_hooks.py
@@ -6,4 +6,4 @@ from wagtail.wagtailcore import hooks
 @hooks.register('insert_editor_css')
 def editor_css():
     return format_html('<link rel="stylesheet" href="{0}'
-        '/css/admin-streamfield-styles.css">'.format(settings.STATIC_URL))
+        'css/admin-streamfield-styles.css">'.format(settings.STATIC_URL))
