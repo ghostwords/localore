3d2f5b914b9f6e2a3124a0b0111949568d14b758
Alexei <alexeiatyahoodotcom@gmail.com>
2015-11-10 14:59:27 -0500

    Update Juicer site summary item.

--- localore/localore_admin/wagtail_hooks.py
+++ localore/localore_admin/wagtail_hooks.py
@@ -1,15 +1,28 @@
+from django.utils.six.moves.urllib import request as urllib_request
+from django.utils.six.moves.urllib.request import Request
+
 from wagtail.wagtailcore import hooks
 from wagtail.wagtailadmin.site_summary import SummaryItem
+
+import json
 
 
 class JuicerSummaryItem(SummaryItem):
     order = 400
     template = 'localore_admin/site_summary_juicer.html'
 
+    # TODO cache
     def get_context(self):
+        request = Request(
+            'https://www.juicer.io/api/feeds/'
+            'fa-demo/moderated?authentication_token='
+            'kGLT6vTEufYRz_csX8Ud&count=true'
+        )
+        r = urllib_request.urlopen(request)
+        count = json.loads(r.read().decode('utf-8'))['posts']['total_count']
+
         return {
-            # TODO total Juicer posts (awaiting moderation?)
-            'total_posts': 0
+            'total_posts': count
         }
 
 
