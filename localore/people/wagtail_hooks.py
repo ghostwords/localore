befce70e7931f5949a7db10af4bae2cb4c21ba08
Alexei <alexeiatyahoodotcom@gmail.com>
2016-03-09 10:43:35 -0500

    Order people by associated production.

--- localore/people/wagtail_hooks.py
+++ localore/people/wagtail_hooks.py
@@ -9,6 +9,7 @@ class PeopleAdmin(ModelAdmin):
     menu_order = 300
     list_display = ('first_name', 'last_name', 'production', 'role')
     list_filter = ('role', 'production')
+    ordering = ('-production',)
     search_fields = ('first_name', 'last_name', 'biography')
 
 wagtailmodeladmin_register(PeopleAdmin)
