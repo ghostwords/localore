0f69150fefd5b1d357257de2d2ddd6ecbb9713c8
Alexei <alexeiatyahoodotcom@gmail.com>
2016-03-09 11:02:57 -0500

    Combine first and last names when listing people.

--- localore/people/wagtail_hooks.py
+++ localore/people/wagtail_hooks.py
@@ -7,9 +7,18 @@ class PeopleAdmin(ModelAdmin):
     menu_icon = 'group'
     menu_label = 'Team'
     menu_order = 300
-    list_display = ('first_name', 'last_name', 'production', 'role')
+    list_display = ('full_name', 'production', 'role')
     list_filter = ('role', 'production')
     ordering = ('-production',)
-    search_fields = ('first_name', 'last_name', 'biography')
+    search_fields = ('last_name', 'first_name', 'biography')
+
+    # pylint: disable=no-self-use
+    def full_name(self, obj):
+        return "%s %s" % (
+            obj.first_name,
+            obj.last_name
+        )
+    full_name.short_description = 'name'
+    full_name.admin_order_field = 'last_name'
 
 wagtailmodeladmin_register(PeopleAdmin)
