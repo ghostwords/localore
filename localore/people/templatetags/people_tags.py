ef7ff91be1e3a3eb40972f9df2d644ac5a038d2a
Alexei <alexeiatyahoodotcom@gmail.com>
2016-03-15 15:49:01 -0400

    Hide non-live productions on the about team page.

--- localore/people/templatetags/people_tags.py
+++ localore/people/templatetags/people_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Q
+
 from people.models import Person
 
 register = template.Library()
@@ -7,6 +9,8 @@ register = template.Library()
 @register.inclusion_tag('people/tags/people.html', takes_context=True)
 def people(context):
     return {
-        'people': Person.objects.all(),
+        'people': Person.objects.filter(
+            Q(production__live=True) | Q(production__isnull=True)
+        ),
         'request': context['request'],
     }
