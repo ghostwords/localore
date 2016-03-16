ghostwords/localore

commit 990563eaecb468487b1b8f73879ac6db7c75e9d9

Add captioning to images.

localore/localore_admin/migrations/0003_auto_20160316_1646.py
@@ -1,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('localore_admin', '0002_auto_20160316_1444'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='localoreimage',
+            old_name='alt',
+            new_name='alt_text',
+        ),
+    ]

localore/blog/migrations/0016_auto_20160316_1520.py
@@ -1,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0015_auto_20160224_2104'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='blogpage',
+            name='main_image',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='localore_admin.LocaloreImage'),
+        ),
+    ]

localore/blog/models.py
@@ -37,7 +37,7 @@ class QuoteBlock(StructBlock):
 
 class BlogPage(Page):
     main_image = models.ForeignKey(
-        'wagtailimages.Image',
+        'localore_admin.LocaloreImage',
         null=True,
         blank=True,
         on_delete=models.SET_NULL,

localore/localore/settings.py
@@ -231,10 +231,15 @@ MEDIA_URL = '/media/'
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "Localore: Finding America"
+
 WAGTAIL_USAGE_COUNT_ENABLED = True
+
 WAGTAILEMBEDS_EMBED_FINDER = 'localore.embeds.finder'
+
 if env('EMBEDLY_KEY'):
     WAGTAILEMBEDS_EMBEDLY_KEY = env('EMBEDLY_KEY')
+
+WAGTAILIMAGES_IMAGE_MODEL = 'localore_admin.LocaloreImage'
 
 if not DEBUG:
     WAGTAILSEARCH_BACKENDS = {

localore/localore_admin/migrations/0002_auto_20160316_1444.py
@@ -1,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import wagtail.wagtailadmin.taggable
+import wagtail.wagtailimages.models
+import wagtail.wagtailcore.models
+import taggit.managers
+from django.conf import settings
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailcore', '0028_merge'),
+        ('wagtailimages', '0012_copy_image_permissions_to_collections'),
+        ('taggit', '0002_auto_20150616_2121'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('localore_admin', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='LocaloreImage',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('title', models.CharField(max_length=255, verbose_name='title')),
+                ('file', models.ImageField(upload_to=wagtail.wagtailimages.models.get_upload_to, verbose_name='file', width_field='width', height_field='height')),
+                ('width', models.IntegerField(verbose_name='width', editable=False)),
+                ('height', models.IntegerField(verbose_name='height', editable=False)),
+                ('created_at', models.DateTimeField(db_index=True, auto_now_add=True, verbose_name='created at')),
+                ('focal_point_x', models.PositiveIntegerField(blank=True, null=True)),
+                ('focal_point_y', models.PositiveIntegerField(blank=True, null=True)),
+                ('focal_point_width', models.PositiveIntegerField(blank=True, null=True)),
+                ('focal_point_height', models.PositiveIntegerField(blank=True, null=True)),
+                ('file_size', models.PositiveIntegerField(null=True, editable=False)),
+                ('alt', models.CharField(help_text="Describe the image's content and functionality for visually impaired users. Leave this blank in case of decorative images. See <a href='http://webaim.org/articles/gonewild/'>here</a> for more information.", blank=True, max_length=255)),
+                ('collection', models.ForeignKey(default=wagtail.wagtailcore.models.get_root_collection_id, to='wagtailcore.Collection', related_name='+', verbose_name='collection')),
+                ('tags', taggit.managers.TaggableManager(help_text=None, verbose_name='tags', to='taggit.Tag', blank=True, through='taggit.TaggedItem')),
+                ('uploaded_by_user', models.ForeignKey(null=True, verbose_name='uploaded by user', on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, blank=True, editable=False)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model, wagtail.wagtailadmin.taggable.TagSearchable),
+        ),
+        migrations.CreateModel(
+            name='LocaloreRendition',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('file', models.ImageField(upload_to='images', width_field='width', height_field='height')),
+                ('width', models.IntegerField(editable=False)),
+                ('height', models.IntegerField(editable=False)),
+                ('focal_point_key', models.CharField(blank=True, max_length=255, editable=False, default='')),
+                ('filter', models.ForeignKey(to='wagtailimages.Filter', related_name='+')),
+                ('image', models.ForeignKey(to='localore_admin.LocaloreImage', related_name='renditions')),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='localorerendition',
+            unique_together=set([('image', 'filter', 'focal_point_key')]),
+        ),
+    ]

localore/localore_admin/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from django.utils.safestring import mark_safe
+
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailimages.models import (
+    Image,
+    AbstractImage,
+    AbstractRendition
+)
 
+
+# site settings
 
 @register_setting(icon='site')
 class JuicerSettings(BaseSetting):
@@ -18,3 +29,47 @@ class JuicerSettings(BaseSetting):
         FieldPanel('juicer_feed_id'),
         FieldPanel('juicer_auth_token'),
     ]
+
+
+# custom image model
+
+class LocaloreImage(AbstractImage):
+    alt_text = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text=(mark_safe(
+            "Describe the image's content and functionality for visually "
+            "impaired users. Leave this blank in case of decorative images. "
+            "See <a href='http://webaim.org/articles/gonewild/'>here</a> for "
+            "more information."
+        ))
+    )
+
+    admin_form_fields = Image.admin_form_fields + (
+        'alt_text',
+    )
+
+
+class LocaloreRendition(AbstractRendition):
+    image = models.ForeignKey('LocaloreImage', related_name='renditions')
+
+    @property
+    def alt(self):
+        return self.image.alt_text
+
+    class Meta:
+        unique_together = (
+            ('image', 'filter', 'focal_point_key'),
+        )
+
+
+@receiver(pre_delete, sender=LocaloreImage)
+# pylint: disable=unused-argument
+def image_delete(sender, instance, **kwargs):
+    instance.file.delete(False)
+
+
+@receiver(pre_delete, sender=LocaloreRendition)
+# pylint: disable=unused-argument
+def rendition_delete(sender, instance, **kwargs):
+    instance.file.delete(False)

localore/people/migrations/0005_auto_20160316_1520.py
@@ -1,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('people', '0004_auto_20160314_2320'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='person',
+            name='photo',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='localore_admin.LocaloreImage'),
+        ),
+    ]

localore/people/models.py
@@ -18,7 +18,7 @@ class Person(models.Model, index.Indexed):
     role = models.CharField(max_length=255, blank=True)
 
     photo = models.ForeignKey(
-        'wagtailimages.Image',
+        'localore_admin.LocaloreImage',
         null=True,
         on_delete=models.SET_NULL,
         related_name='+'

commit 260cb862f5324387d796826d76ff224c01ca5036

Fix migrations dependency issue caused by 990563e.

localore/localore_admin/migrations/0003_auto_20160316_1646.py
@@ -10,6 +10,10 @@ class Migration(migrations.Migration):
         ('localore_admin', '0002_auto_20160316_1444'),
     ]
 
+    run_before = [
+        ('home', '0002_create_homepage'),
+    ]
+
     operations = [
         migrations.RenameField(
             model_name='localoreimage',