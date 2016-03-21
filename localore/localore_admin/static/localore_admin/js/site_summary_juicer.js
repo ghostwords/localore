bc7b85e5272220b2f2e368a7639526f3f448b4d3
Alexei <alexeiatyahoodotcom@gmail.com>
2016-03-21 08:47:05 -0400

    Make Juicer site summary item async.

---
+++ localore/localore_admin/static/localore_admin/js/site_summary_juicer.js
@@ -0,0 +1,8 @@
+$.ajax($('#juicer-summary-item')
+	.data('juicer-summary-item-url'))
+	.done(function (data) {
+		$('#juicer-summary-item')
+			.removeClass('icon-spinner')
+			.addClass('icon-site')
+			.append(data);
+	});
