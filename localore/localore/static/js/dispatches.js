ff9d5b432c0a9172916873b8346d0c8e6cbd44a7
Dan Kass <romeboards@gmail.com>
2016-04-04 20:23:30 -0400

    dispatches prev/next

--- localore/localore/static/js/dispatches.js
+++ localore/localore/static/js/dispatches.js
@@ -2,16 +2,23 @@ $(function() {
 
   var $modal = $('#dispatch');
   var $modalBody = $modal.find('.modal-body');
+  var $prev = $modal.find('.dispatch-prev');
+  var $next = $modal.find('.dispatch-next');
 
-  $('.post-item-link').on('click', function() {
-    var $this = $(this);
-    var item = $this.data().item;
+  var loadDispatch = function(data) {
+    $next.removeClass('show');
+    $prev.removeClass('show');
+    if(data.prev_url) $prev.addClass('show').attr("data-item", data.prev_url);
+    if(data.next_url) $next.addClass('show').attr("data-item", data.next_url);
+    $modalBody.html(data.embed_html);
+    $modal.modal('show');
+  };
 
+  $('.post-item-link, .dispatch-arrow').on('click', function() {
+    var item = $(this).attr("data-item");
     $.getJSON(item, function(data) {
-      $modalBody.html(data.embed_html);
-      $modal.modal('show');
+      loadDispatch(data);
     });
-
   });
 
 });
