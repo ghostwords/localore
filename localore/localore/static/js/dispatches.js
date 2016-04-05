00af468541187f9766f53d1407847bc27099ef7f
Dan Kass <romeboards@gmail.com>
2016-04-04 21:25:21 -0400

    load dispatch from URL

--- localore/localore/static/js/dispatches.js
+++ localore/localore/static/js/dispatches.js
@@ -14,15 +14,38 @@ $(function() {
     $modal.modal('show');
   };
 
+  var getUrlParameter = function(sParam) {
+      var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+          sURLVariables = sPageURL.split('&'),
+          sParameterName,
+          i;
+
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+
+          if (sParameterName[0] === sParam) {
+              return sParameterName[1] === undefined ? true : sParameterName[1];
+          }
+      }
+  };
+
+  var item = getUrlParameter("dispatch");
+  if(item) {
+    $.getJSON(item + "?json", function(data) { loadDispatch(data); });
+  }
+
+
+
   $('.post-item-link, .dispatch-arrow').on('click', function() {
     var item = $(this).attr("data-item");
-    $.getJSON(item, function(data) {
-      loadDispatch(data);
-    });
+    $.getJSON(item, function(data) { loadDispatch(data); });
   });
 
   $modal.on('hide.bs.modal', function (e) {
     $modalBody.empty();
   });
+
+
+
 
 });
