9bbae52402ebb9b1c29871b5f18accdc932fc9ec
Dan Kass <romeboards@gmail.com>
2016-04-04 22:05:22 -0400

    keydown events

--- localore/localore/static/js/dispatches.js
+++ localore/localore/static/js/dispatches.js
@@ -6,12 +6,33 @@ $(function() {
   var $next = $modal.find('.dispatch-next');
 
   var loadDispatch = function(data, item) {
+
+    // clear prev/next arrows and apply new hrefs
     $next.removeClass('show');
     $prev.removeClass('show');
     if(data.prev_url) $prev.addClass('show').attr("href", data.prev_url);
     if(data.next_url) $next.addClass('show').attr("href", data.next_url);
+
+    // set keydown events
+    $(document).on("keydown.dispatch", function(evt) {
+      evt = evt || window.event;
+      switch (evt.keyCode) {
+          case 37:
+              if($prev.hasClass('show')) $prev.click();
+              break;
+          case 39:
+              if($next.hasClass('show')) $next.click();
+              break;
+      }
+    });
+
+    // change URL
     window.history.pushState("", data.title, item);
+
+    // embed new content
     $modalBody.html(data.embed_html);
+
+    // show the modal
     $modal.modal('show');
   };
 
@@ -42,8 +63,10 @@ $(function() {
   });
 
   $modal.on('hide.bs.modal', function (e) {
+    $(document).off("keydown.dispatch");
     $modalBody.empty();
   });
+
 
 
 
