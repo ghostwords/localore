c2426cead4db3748fb2c4e9e7f45457ea1de00aa
Dan Kass <romeboards@gmail.com>
2016-04-04 21:37:56 -0400

    dispatch URL

--- localore/localore/static/js/dispatches.js
+++ localore/localore/static/js/dispatches.js
@@ -5,11 +5,12 @@ $(function() {
   var $prev = $modal.find('.dispatch-prev');
   var $next = $modal.find('.dispatch-next');
 
-  var loadDispatch = function(data) {
+  var loadDispatch = function(data, item) {
     $next.removeClass('show');
     $prev.removeClass('show');
     if(data.prev_url) $prev.addClass('show').attr("data-item", data.prev_url);
     if(data.next_url) $next.addClass('show').attr("data-item", data.next_url);
+    window.history.pushState("", data.title, item);
     $modalBody.html(data.embed_html);
     $modal.modal('show');
   };
@@ -31,14 +32,14 @@ $(function() {
 
   var item = getUrlParameter("dispatch");
   if(item) {
-    $.getJSON(item + "?json", function(data) { loadDispatch(data); });
+    $.getJSON(item + "?json", function(data) { loadDispatch(data, item); });
   }
 
 
 
   $('.post-item-link, .dispatch-arrow').on('click', function() {
     var item = $(this).attr("data-item");
-    $.getJSON(item, function(data) { loadDispatch(data); });
+    $.getJSON(item, function(data) { loadDispatch(data, item); });
   });
 
   $modal.on('hide.bs.modal', function (e) {
