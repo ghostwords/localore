64f17c8eab50583e9039f8f70f491cd4fecb3201
Alexei <alexeiatyahoodotcom@gmail.com>
2016-12-01 10:00:33 -0500

    Fix error on blog pages with unpublished parent.

--- localore/blog/templatetags/blog_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from blog.models import BlogIndexPage
-
-register = template.Library()
-
-
-@register.assignment_tag
-def get_blog_index_page():
-    return BlogIndexPage.objects.live().first()
